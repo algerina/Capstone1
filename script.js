e95781378dec07f9d335c643e980bc6ce782b5e2
Amel Khiri <53347966+algerina@users.noreply.github.com>
2021-09-16 00:45:43 +0100

    Create home page html for mobile version

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mobileSidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mobileSidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
